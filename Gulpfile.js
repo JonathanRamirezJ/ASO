98ec17251df9ce7290585dd66964039a29b9dcd5
JonathanRamirez <jonajramirez@gmail.com>
2016-09-17 04:33:41 -0500

    Jonathan: Adds complement files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,15 @@
+// File: Gulpfile.js
+'use strict';
+
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    historyApiFallback = require('connect-history-api-fallback');
+    stylus = require('gulp-stylus'),
+    nib = require('nib'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    historyApiFallback = require('connect-history-api-fallback'),
+    inject = require('gulp-inject'),
+    wiredep = require('wiredep').stream;
 
 // Servidor web de desarrollo
 gulp.task('server', function() {
@@ -17,8 +26,13 @@ gulp.task('server', function() {
   });
 });
 
-var stylus = require('gulp-stylus'),
-    nib = require('nib');
+// Busca errores en el JS y nos los muestra por pantalla
+gulp.task('jshint', function() {
+ return gulp.src('./app/scripts/main.js')
+   .pipe(jshint('.jshintrc'))
+   .pipe(jshint.reporter('jshint-stylish'))
+   .pipe(jshint.reporter('fail'));
+});
 
 // Preprocesa archivos Stylus a CSS y recarga los cambios
 gulp.task('css', function() {
@@ -34,12 +48,34 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el index.html
+gulp.task('inject', function() {
+ var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css']);
+ return gulp.src('index.html', {cwd: './app'})
+  .pipe(inject(sources, {
+    ignorePath: '/app'
+  }))
+  .pipe(gulp.dest('./app'));
+});
+
+// Inyecta las librerias que instalemos vía Bower
+gulp.task('wiredep', function () {
+ gulp.src('./app/index.html')
+  .pipe(wiredep({
+    directory: './app/lib'
+  }))
+  .pipe(gulp.dest('./app'));
+});
+
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
  gulp.watch(['./app/**/*.html'], ['html']);
- gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+ gulp.watch(['./app/stylesheets/**/*.styl'], ['css','inject']);
+ gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint','inject']);
+ gulp.watch(['./bower.json'], ['wiredep']);
 });
 
 //Comando para iniciar todas las tareas.
-gulp.task('serve', ['server', 'watch']);
+gulp.task('serve', ['server', 'inject', 'wiredep', 'watch']);
---
+++ app/scripts/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function() {
+ console.log('ASO run in: http://127.0.0.1:8080');
+})();
